ca5101d98c6f65903ffc3567141790749db58a52
jykrasolutions <jykrasolutions@Jykra-PC1>
2022-01-24 18:30:55 +0530

    pyramid second way program modified in a simplest form

--- NewBeginning/src/startagain/pyramidsecondway.java
+++ NewBeginning/src/startagain/pyramidsecondway.java
@@ -7,13 +7,12 @@ public class pyramidsecondway
 	int i,j; int k = 0;
 	public void secondway(int num) 
 	{
-		for (i = 1; i <= num; ++i)
+		for (i = 1; i <= num; i++,k=0)
 		{
 		      for (j = 1; j <= num - i; ++j) 
 		      {
 		        System.out.print("  ");
 		      }
-		      //System.out.println(i);
 		      while (k != 2 * i - 1) 
 		      {
 		    	  System.out.print("* ");
